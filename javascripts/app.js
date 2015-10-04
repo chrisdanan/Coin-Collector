f638a2f07ba3b05615b98e5ad2146c6223e40200
Christopher Danan <chrisddan@gmail.com>
2015-10-03 17:07:44 -0700

    Create a way to animate the sprites by using javascript.

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,20 @@ var main = function(){
 	"use strict";
 
 	console.log("Hello Vane");
+
+	var coin = $("#coin");
+	var i = 1;
+
+	window.setInterval(function(){
+		coin.removeClass();
+		coin.addClass("coin_" + i);
+
+		i++;
+		if(i > 6){
+			i = 1;
+		}
+
+	}, 100);
 };
 
 $(document).ready(main);

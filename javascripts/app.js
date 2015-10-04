f5c6bff1f552a73e8891ec93a86bf2950324a3ae
Christopher Danan <chrisddan@gmail.com>
2015-10-04 14:26:38 -0700

    Create code that handles collecting coins with the mouse: -The coin disappears as soon as the mouse collides with it. -The coin appears about 3 seconds later.

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,21 @@ var main = function(){
 	"use strict";
 
 	console.log("Hello Vane");
+
+	//Trigger mouse collision with coin.
+	$(".coin").mouseenter(function(){
+		var coin = $(this);
+
+		//Hide coin right away once mouse touches it.
+		window.setTimeout(function(){
+			coin.hide();
+
+			//After about 3 seconds, show the coin again.
+			window.setTimeout(function(){
+				coin.show();
+			}, 3333);
+		}, 0);
+	});
 };
 
 $(document).ready(main);

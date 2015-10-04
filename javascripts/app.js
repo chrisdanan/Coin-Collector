906e90ba97aeab1ea5051ba0f7fb364ebccf1bed
Christopher Danan <chrisddan@gmail.com>
2015-10-03 17:34:32 -0700

    Remove javascript way of animating and add in 100 coins to the project: NOTES: * I removed javascript way because I couldn't think of a method for animating more than one object. I will work on that soon. * I will create a php version of the project, that way I don't have to have 100 lines of <div> for the coins.

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,24 +2,6 @@ var main = function(){
 	"use strict";
 
 	console.log("Hello Vane");
-
-	/*
-	var coin = $("#coin");
-	var i = 1;
-
-	window.setInterval(function(){
-		coin.removeClass();
-		coin.addClass("coin_" + i);
-
-		i++;
-		if(i > 6){
-			i = 1;
-		}
-
-	}, 100);
-	*/
-
-	
 };
 
 $(document).ready(main);

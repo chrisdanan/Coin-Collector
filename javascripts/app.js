5339387021601a43194767c16e06ce13efb1266c
Christopher Danan <chrisddan@gmail.com>
2015-10-05 16:11:48 -0700

    Add coin sound effect.

--- javascripts/app.js
+++ javascripts/app.js
@@ -9,6 +9,10 @@ var main = function(){
 
 		//Hide coin right away once mouse touches it.
 		window.setTimeout(function(){
+			//Reference: http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
+			var coinSound = new Audio("assets/smw_coin.wav");
+
+			coinSound.play();
 			coin.hide();
 
 			//After about 3 seconds, show the coin again.

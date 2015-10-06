caaa17fab9145625850d2c408a665e6881ec6916
Christopher Danan <chrisddan@gmail.com>
2015-10-06 10:06:53 -0700

    Keep track of the total number of coins the player has collected and keep track of the number of coins left on the grid. If the number of coins left on the grid has reached 0 (i.e. the grid is empty), then an Easter egg will show.

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,26 +3,44 @@ var main = function(){
 
 	console.log("Hello Vane");
 
+	var coinsCollected = 0,						//Keep track of number of coins collected by player.
+		gridCount = $(".coin").length;			//Keep track of number of coins left on the grid.
+
+	$(".num-coins").text(coinsCollected);		//Initialize coin counter to 0.
+
 	//Trigger mouse collision with coin.
 	$(".coin").mouseenter(function(){
 		var coin = $(this);
+
+		coin.removeClass("coin");									//Hide coin.
+
+		coinsCollected++;											//Increase coin counter by 1.
+		$(".num-coins").text(coinsCollected);						//Update coin counter.
 
 		//Hide coin right away once mouse touches it.
 		window.setTimeout(function(){
 			//Reference: http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
 			var coinSound = new Audio("assets/smw_coin.wav");
 
-			coinSound.play();
-			//coin.hide();
-			coin.removeClass("coin");								//Hide coin.
 			coin.addClass("coin-collected");						//Show coin-collected animation.
+
+			gridCount = $(".coin").length;							//Update number of coins left on grid (i.e. decrease count).
+
+			if(gridCount === 0){									//If player collected all coins on the grid, then show Easter egg.
+				console.log("you collected them all!");
+			}
+
+			coinSound.play();
 
 			window.setTimeout(function(){
 				coin.removeClass("coin-collected");					//Remove coin-collected animation.
 
 				window.setTimeout(function(){
-					//coin.show();
-					coin.addClass("coin");							//After about 3 seconds, show the coin again.
+					coin.addClass("coin");							//After 7 seconds, show the coin again.
+																	//7 seconds gives the player enough time to collect all coins, but
+																	// is short enough to make it challenging.
+
+					gridCount = $(".coin").length;					//Update number of coins left on grid (i.e. increase count).
 				}, 7000);
 			},300);
 		}, 0);

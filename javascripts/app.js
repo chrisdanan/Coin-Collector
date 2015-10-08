8119ea6d9442f54fa36b1c5b33d500d979e5dc25
Christopher Danan <chrisddan@gmail.com>
2015-10-07 23:52:52 -0700

    Add commentary at certain points of the game and fix bug with coin-collected animation: -Commentary:  * After collecting certain amounts of coins, commentary shows up in the #commentary paragraph of the project. -Coin-collected animation bug:  * Since fixing the previous bug by using .detach(), the coin-collected animation was unable to play.  * Fix the bug by creating a new <div> with the animation, appending it to empty coin-container, then removing it before adding the coin back in.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,166 @@
+var commentary = function(coinCount){
+	switch(coinCount){
+		case 3:
+			$("#commentary").text("Oh, hello there.");
+			break;
+		case 10:
+			$("#commentary").text("Hey, those are my coins!");
+			break;
+		case 50:
+			$("#commentary").text("This is very rude of you.");
+			break;
+		case 100:
+			$("#commentary").text("How would you like it if I stole all of your shiny coins, huh?!");
+			break;
+		case 111:
+			$("#commentary").text("Are you even listening to me?");
+			break;
+		case 150:
+			$("#commentary").text("You really like money that much?");
+			break;
+		case 200:
+			$("#commentary").text("What will you even do with all of that money?");
+			break;
+		case 230:
+			$("#commentary").text("Hmm... maybe this is some sort of obsessive compulsive disorder...");
+			break;
+		case 260:
+			$("#commentary").text("Okay, I'm going to test you. Do NOT collect another coin. Not even one!!!");
+			break;
+		case 261:
+			$("#commentary").text("Hmm, so you really can't control yourself. That's a shame.");
+			break;
+		case 290:
+			$("#commentary").text("Or maybe... could it be you're doing this to annoy me?");
+			break;
+		case 333:
+			$("#commentary").text("Okay then, if that's the way it's going to be, I'm just going to ignore you!!!");
+			break;
+		case 355:
+			$("#commentary").text("...");
+			break;
+		case 400:
+			$("#commentary").text("STOP THAT!!!!!!");
+			break;
+		case 420:
+			$("#commentary").text("You're a sick person.");
+			break;
+		case 450:
+			$("#commentary").text("I can't believe you're still here.");
+			break;
+		case 500:
+			$("#commentary").text("Wow, 500 coins... You do realize you could have been doing something productive this whole time.");
+			break;
+		case 550:
+			$("#commentary").text("You know, I think you should stop now.");
+			break;
+		case 600:
+			$("#commentary").text("Yeah, you really should stop. It's for your own good.");
+			break;
+		case 650:
+			$("#commentary").text("Itigil!");
+			break;
+		case 700:
+			$("#commentary").text("You really really should stop playing now!");
+			break;
+		case 800:
+			$("#commentary").text("You know what, fine. Have it your way. I'm out.");
+			break;
+		case 825:
+			$("#commentary").text("Goodbye, so long, farewell, and...");
+			break;
+		case 850:
+			$("#commentary").text("GOOD RIDDANCE!!!");
+			break;
+		case 875:
+			$("#commentary").text("");
+			break;
+		case 1000:
+			$("#commentary").text("YOU'RE STILL HERE?!?!?!");
+			break;
+		case 1025:
+			$("#commentary").text("You're so desperate. Or is it curiousity?");
+			break;
+		case 1050:
+			$("#commentary").text("There really is no end. Nothing. Nada. No pot at the end of the rainbow.");
+			break;
+		case 1100:
+			$("#commentary").text("This is a never ending game. Like Cookie Clicker! Only not as in-depth!");
+			break;
+		case 1111:
+			$("#commentary").text("Fine then, keep collecting coins. I'll just watch.");
+			break;
+		case 1150:
+			$("#commentary").text("1150");
+			break;
+		case 1151:
+			$("#commentary").text("1151");
+			break;
+		case 1152:
+			$("#commentary").text("1152...");
+			break;
+		case 1153:
+			$("#commentary").text("1153 (I can do this all day too, you know)");
+			break;
+		case 1154:
+			$("#commentary").text("Actually, I'm bored already. How are you still doing this?");
+			break;
+		case 1175:
+			$("#commentary").text("Let's play a new game. How about this? I think of a word, and you guess what I'm thinking?");
+			break;
+		case 1200:
+			$("#commentary").text("Ready?");
+			break;
+		case 1225:
+			$("#commentary").text("HIPPO");
+			break;
+		case 1235:
+			$("#commentary").text("Did you get it?");
+			break;
+		case 1250:
+			$("#commentary").text("Okay, how about anoth- oh wait, hold on...");
+			break;
+		case 1260:
+			$("#commentary").text("Okay, yes");
+			break;
+		case 1270:
+			$("#commentary").text("Yes sir.");
+			break;
+		case 1280:
+			$("#commentary").text("Right away sir.");
+			break;
+		case 1290:
+			$("#commentary").text("Will do sir, thank you sir.");
+			break;
+		case 1300:
+			$("#commentary").text("AHEM. That was the President. I'm called away for urgent duty. By the time I get back, you better be gone.");
+			break;
+		case 1310:
+			$("#commentary").text("Well then... it was a... pleasure? Nono, it was a...");
+			break;
+		case 1320:
+			$("#commentary").text("It was a headache to get to know you.");
+			break;
+		case 1330:
+			$("#commentary").text("I'm off now, goodbye again. And this time, it's for real.");
+			break;
+		case 1333:
+			$("#commentary").text("*Door opens and shuts loudly. You hear a car start and rattle off into the distance.*");
+			break;
+		case 1350:
+			$("#commentary").text("*You are alone.*");
+			break;
+		case 1400:
+			$("#commentary").text("");
+			break;
+		case 10000:
+			$("#commentary").text("Ahh, home at la- WHAT THE F***");
+			break;
+		default:
+			break;
+	}
+};
+
 var main = function(){
 	"use strict";
 
@@ -21,13 +184,15 @@ var main = function(){
 
 		coinsCollected++;											//Increase coin counter by 1.
 		$(".num-coins").text(coinsCollected);						//Update coin counter.
+		commentary(coinsCollected);
 
 		//Hide coin right away once mouse touches it.
 		window.setTimeout(function(){
 			//Reference: http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
 			var coinSound = new Audio("assets/smw_coin.wav");
 
-			coin.addClass("coin-collected");						//Show coin-collected animation.
+			var coinCollectedAnim = $("<div>").addClass("coin-collected");	//Create coin collected animation.
+			coinContainer.append(coinCollectedAnim);						//Append the coin collected animation to the coin container of the coin that was just collected.
 
 			gridCount = $(".coin").length;							//Update number of coins left on grid (i.e. decrease count).
 
@@ -38,8 +203,8 @@ var main = function(){
 			coinSound.play();
 
 			window.setTimeout(function(){
+				coinCollectedAnim.remove();							//Remove the coin collected animation.
 				coinContainer.append(coin);							//Add the coin back into its container.
-				coin.removeClass("coin-collected");					//Remove coin-collected animation.
 
 				window.setTimeout(function(){
 					coin.addClass("coin");							//After 7 seconds, show the coin again.
